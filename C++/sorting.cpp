c889956e51282cd2585a92ca6e15f6e295366ad7
parthshah31 <shahp@mit.edu>
2016-07-18 22:56:08 -0700

    Merge Sort

--- C++/sorting.cpp
+++ C++/sorting.cpp
@@ -2,9 +2,6 @@
 #include <cstdlib>
 #include <assert.h>
 
-//template methods for comparison based sorting
-//counting sort for integer sorting
-
 template <class T>
 bool LessThan(const T& a, const T& b){
     return (a < b);
@@ -13,7 +10,7 @@ bool LessThan(const T& a, const T& b){
 /*
  QUICKSORT
  In nemory quicksort algorithm. Setting randomized to true makes it in memory randomized quicksort algorithm
- 
+ NOTE: Current implementation is not a stable sort
  Uses Partition Method Defined Below
  */
 template <class T>
@@ -73,7 +70,58 @@ int Partition(T* array[], int start, int end, bool randomized){
 
 /*
  MERGE SORT
- Merge Sort method
+ Merge Sort method. Not in memory. Stable sort.
  */
-//template <class T>
-//void
+template <class T>
+T* MergeSort(T* array[]){
+    int size = sizeof(*array);
+    if(size < 2){
+        return array;
+    }
+    T array1[size/2];
+    T array2[size - size/2];
+    T sorted1[size/2];
+    T sorted2[size - size/2];
+    for(int i = 0; i < size/2; i++){
+        array1[i] = *array[i];
+    }
+    for(int i = size/2; i < size; i++){
+        array1[i - size/2] = *array[i];
+    }
+    sorted1 = MergeSort(array1);
+    sorted2 = MergeSort(array2);
+    return *Merge(sorted1, sorted2);
+}
+
+template <class T>
+T* Merge(T* array1[], T* array2[]){
+    int size = sizeof(*array1) + sizeof(*array2);
+    T array[size];
+    int index;
+    int index1 = 0;
+    int index2 = 0;
+    while(index1 < sizeof(*array1) && index2 < sizeof(*array2)){
+        if(LessThan(*array1[index1],*array2[index2])){
+            array[index++] = *array1[index1++];
+        }
+        else{
+            array[index++] = *array2[index2++];
+        }
+    }
+    if(index1 == sizeof(*array1)){
+        for(int i = index2; i < sizeof(*array2); i++){
+            array[index++] = *array2[index2++];
+        }
+    }
+    else{
+        for(int i = index1; i < sizeof(*array1); i++){
+            array[index++] = *array2[index1++];
+        }
+    }
+    return *array;
+}
+
+/*
+ COUNTING SORT/RADIX SORT
+ Counting Sort/Radix Sort Method. Stable Sort
+ */
--- C++/sorting.hpp
+++ C++/sorting.hpp
@@ -10,4 +10,10 @@ void QuickSort(T* array[], int start = 0, int end = -1, bool randomized = false)
 template <class T>
 int Partition(T* array[], int start, int end, bool randomized);
 
+template <class T>
+T* MergeSort(T* array[]);
+
+template <class T>
+T* Merge(T* array1[], T* array2[]);
+
 #endif
